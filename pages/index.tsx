5bc564e52b326e1737137fcb5816045fd05c35fb
ryota.doi <ryota.doi.1102@gmail.com>
2021-12-01 11:23:29 +0900

    :lipstick: Update home page UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,64 @@
+import { Avatar, Card, CardHeader, CardMedia, List, ListItem } from '@mui/material';
 import { NextPage } from 'next';
 import React from 'react';
 
-const Home: NextPage = () => {
+type Movie = {
+  id: string;
+  title: string;
+  channelId: string;
+  avatarUrl: string;
+  thumbnailUrl: string;
+  createdAt: number;
+};
+
+type Props = {
+  movies: Movie[];
+};
+
+const MovieList = (props: Props) => {
+  return (
+    <List sx={{ pl: 1, pr: 1 }}>
+      {props.movies.map((movie) => (
+        <React.Fragment key={movie.id}>
+          <ListItem disablePadding>
+            <Card>
+              <CardMedia image={movie.thumbnailUrl} height={194} component={'img'} />
+              <CardHeader avatar={<Avatar>s</Avatar>} title={movie.title} subheader={movie.createdAt} />
+            </Card>
+          </ListItem>
+        </React.Fragment>
+      ))}
+    </List>
+  );
+};
+
+const a: Movie[] = [
+  {
+    id: 'hogehoge',
+    title: 'こんにちは',
+    channelId: 'gehogeho',
+    avatarUrl: '',
+    thumbnailUrl:
+      'https://i.ytimg.com/vi/B_ncQhqfHas/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwrZyhtIkSy80xxx3hnhOS53_vqg',
+    createdAt: 156783,
+  },
+  {
+    id: 'hogehoge',
+    title: 'こんにちは',
+    channelId: 'gehogeho',
+    avatarUrl: '',
+    thumbnailUrl:
+      'https://i.ytimg.com/vi/B_ncQhqfHas/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwrZyhtIkSy80xxx3hnhOS53_vqg',
+    createdAt: 156783,
+  },
+];
+
+const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hello!!</h1>
+      <MovieList movies={a} />
     </>
   );
 };
 
-export default Home;
+export default HomePage;
